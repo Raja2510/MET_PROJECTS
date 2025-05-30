cd86f20d7901df3691898215d70280650a2ab99a
Raja251 <rajasimha.2510@gamil.com>
2025-05-30 19:31:21 +0530

    1

---
+++ python_project-1.py
@@ -0,0 +1,17 @@
+members={
+    
+}
+project={
+    "p1":{
+        "name":"project",
+        "start_date":"30-05",
+        "end_date":"30-06",
+        "priority":1,
+        "assigned_members":{
+            
+        }
+    }
+
+}
+
+
