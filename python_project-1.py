c61ed5edf355d94b363be2856e43babd59c683d1
Raja251 <rajasimha.2510@gamil.com>
2025-05-31 01:30:41 +0530

    30-05

--- python_project-1.py
+++ python_project-1.py
@@ -1,18 +1,27 @@
-members={ 
+members={"m1":{"name":"raja",
+               "role":"coding",
+               "skill_set":"python",
+               "hourly_rate":"6%"}} 
 
-}
+
 project={
     "p1":{
         "name":"project",
         "start_date":"30-05",
         "end_date":"30-06",
         "priority":1,
-        "task":{}
+        
             
         }
     }
 
+task={"t1":{"title":"task-1",
+                      "description":"complete task",
+                      "estimates_hours":"10 hours"}
+}
 
+
+##----adding project---------------------
 def create_project(project_id, name, start_date, end_date, priority):
     if project_id in project:
         return "this project already exsists"
@@ -21,6 +30,7 @@ def create_project(project_id, name, start_date, end_date, priority):
     print("project added sucessfully")
 
 
+##----adding members---------------------
 
 def add_team_member(member_id, name, role, skill_set, hourly_rate) :
     if member_id in members:
@@ -41,3 +51,7 @@ def add_team_member(member_id, name, role, skill_set, hourly_rate) :
 #     create_project(pid,na,sd,ed,prio)
 # print(project)
 
+def create_task(task_id, project_id, title, description, estimated_hours) :
+    if task_id in task:
+        return "this task already exist" 
+    task.update({task_id:{"title":title,"description":description,"estimated_hours":estimated_hours}})
