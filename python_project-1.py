f72be7f9bd1db6a793a2316107f5a163bad37f26
Raja251 <rajasimha.2510@gamil.com>
2025-06-02 17:10:33 +0530

    first commit

--- python_project-1.py
+++ python_project-1.py
@@ -10,14 +10,15 @@ project={
         "start_date":"30-05",
         "end_date":"30-06",
         "priority":1,
-        
+        "status":"active"
             
         }
     }
 
 task={"t1":{"title":"task-1",
                       "description":"complete task",
-                      "estimates_hours":"10 hours"}
+                      "estimates_hours":"10 hours",
+                      "project_id":"p1"}
 }
 
 
@@ -41,17 +42,50 @@ def add_team_member(member_id, name, role, skill_set, hourly_rate) :
 
 
 
-# a =input("1.add project :")
-# if a == "yes":
-#     pid=input("enter project name")
-#     na=input("enter name")
-#     sd=input("sarte date")
-#     ed=input("enter end date")
-#     prio=input("entrt prio")
-#     create_project(pid,na,sd,ed,prio)
-# print(project)
-
 def create_task(task_id, project_id, title, description, estimated_hours) :
     if task_id in task:
         return "this task already exist" 
-    task.update({task_id:{"title":title,"description":description,"estimated_hours":estimated_hours}})
+    task.update({task_id:{"title":title,"description":description,"estimated_hours":estimated_hours,"project_id":project_id}})
+
+
+
+
+while True:
+    print("""
+    -------Task Manager -MENU----------------------------:
+    Show projects.------------1
+    Create a new project.-----2
+    Add new member.-----------3
+    Create a new task.--------4
+    Exit----------------------5
+    """)
+    option=int(input("Enter number corresponding to the action you want to perfrom : "))
+
+    if option==5:
+        print("Closing Task Manager")
+        break
+    elif option==1:
+        pid=input("Enter project name : ")
+        na=input("Enter name : ")
+        sd=input("Enter start Date : ")
+        ed=input("Enter end Date : ")
+        prio=input("Enter priority : ")
+        create_project(pid,na,sd,ed,prio)
+        print(project)
+    elif option==2:
+        m_id=input("Enter Member ID : ")
+        m_name=input("Enter member name : ")
+        m_rl=input("Enter role : ")
+        m_ss=input("Enter Skill set : ")
+        m_hr=input("Enter hourly rate : ")
+        add_team_member(m_id,m_name,m_rl,m_ss,m_hr)
+        print(members)
+    elif option==3:
+        t_id=input("Enter Task name : ")
+        t_pid=input("Enter Project_id")
+        t_tit=input("Enter Title")
+        t_des=input("Enter description")
+        t_eh=input("Estimated hours")
+        create_task(t_id,t_pid,t_tit,t_des,t_eh)
+        print(task)
+       
